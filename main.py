8effa03b693559acfff65114e7bb1f308ef61add
MatteoRI <49910316+MatteoRI@users.noreply.github.com>
2021-07-02 08:24:52 -0400

    Added ability for user to enter fraction

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 from ExponentialFunction import ExponentialFunction
 
+# Start with simple ExponentialFunction option where all values are 0
 function = ExponentialFunction(0,0,0,0)
+
+# Display format of Exponential Function
 print('ab' + ExponentialFunction.get_super('x'))
+
+# Ask the user to input the value for a
 mNum = input("Enter value for a: ")
-function.set_Mult_Num(float(mNum))
+
+# If the user enters a fraction, then split into numerator and denominator
+# And pass the value of num/den in set_mult_num
+if '/' in mNum:
+    num, den = mNum.split('/')
+    function.set_mult_num(float(num)/float(den))
+
+# If the user enters an integer, then pass it as integer in set_mult_num
+# Determine if it's an integer using isdigit method
+elif mNum.isdigit():
+    function.set_mult_num(int(mNum))
+
+# Finally, if mNum is decimal, then pass it as float(mNum)
+else :
+    function.set_mult_num(float(mNum))
+
+# Then ask the user to enter the value for b, and perform the same checks
 bNum = input("Enter value for b: ")
-function.set_Base_Num(float(bNum))
+
+if '/' in bNum:
+    num, den = bNum.split('/')
+    function.set_base_num(float(num)/float(den))
+    
+elif bNum.isdigit():
+    function.set_base_num(int(bNum))
+    
+else :
+    function.set_mult_num(float(bNum))
+
+# Ask the user to enter the value for x
 eNum = input("Enter value for x: ")
-function.set_Exp_Num(float(eNum))
-print(function.calculateAnswer())
+function.set_exp_num(int(eNum))
+
+print(function.calculate_answer())
 print(function)
