7e4e6186638895abd34a9d28e4f31ca732385c95
MatteoRI <49910316+MatteoRI@users.noreply.github.com>
2021-07-02 12:01:10 -0400

    Updated ExponentialFunction.py

--- main.py
+++ main.py
@@ -15,12 +15,7 @@ if '/' in mNum:
     num, den = mNum.split('/')
     function.set_mult_num(float(num)/float(den))
 
-# If the user enters an integer, then pass it as integer in set_mult_num
-# Determine if it's an integer using isdigit method
-elif mNum.isdigit():
-    function.set_mult_num(int(mNum))
-
-# Finally, if mNum is decimal, then pass it as float(mNum)
+# Else, pass mNum as float
 else :
     function.set_mult_num(float(mNum))
 
@@ -31,15 +26,12 @@ if '/' in bNum:
     num, den = bNum.split('/')
     function.set_base_num(float(num)/float(den))
     
-elif bNum.isdigit():
-    function.set_base_num(int(bNum))
-    
 else :
-    function.set_mult_num(float(bNum))
+    function.set_base_num(float(bNum))
 
 # Ask the user to enter the value for x
 eNum = input("Enter value for x: ")
-function.set_exp_num(int(eNum))
+function.set_exp_num(eNum)
 
 print(function.calculate_answer())
 print(function)
