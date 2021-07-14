7a7a8b13e1b05b957c091e2b256513e76903306c
Proxy-MK <31321406+Proxy-MK@users.noreply.github.com>
2021-07-14 19:45:32 -0400

    Updated
    - Updated REGEX
    - Updated methodology (WIP)
        => It will only parse functions to get literal values, then use Python's innate expression parser (eval()) to complete the basic mathematical process (+, -, *, / )

--- source_code/parser.py
+++ source_code/parser.py
@@ -1,6 +1,16 @@
 import re
+import ast
+
 
 # (((Sin(22) + Cos(332)) * 2 + Cos(Cos(932))) / 5)/ Sin(666)
+"""
+fnc(#)+#
+(fnc(#)+#)
+fnc(fnc(#) + #)
+
+
+
+"""
 # As of now, it only works with integers, float support will be added later
 
 
@@ -9,62 +19,95 @@ import re
 
 sinRegex = re.compile(r'(?:sin\(\d*\))')
 cosRegex = re.compile(r'(?:cos\(\d*\))')
-priorityRegex = re.compile(r'(?<!^sin)(?<!^cos)(\(.*?\))')
+cRegex = re.compile(r'\((.*?)\)')
+
+
+
+
+
+
+
+priorityRegex = re.compile(r'(?<=\()(.*?)(?=\))')
 literalRegex = re.compile(r'^[0-9]*$')
 
 def parseEqu(s):
 
-    print("Input String: " + s)
+    _local_string = str(s)
+
+    print("Input String: " + _local_string)
 
     # Compute functions with values
-    if sinRegex.match(s):
+    if sinRegex.findall(_local_string):
         print("checkpoint_1")
-        matc = sinRegex.search(s)
-        print("(SIN) Handling match: " + matc.group())
-        return "-1"
-        pass 
-    elif cosRegex.match(s):
+        matc = sinRegex.findall(_local_string)
+        for matches in matc:
+           _local_string = sinRegex.sub("-1", _local_string)
+        _local_string = parseEqu(_local_string)
+        print("(SIN) Handling match: " + str(matc))
+
+
+    if cosRegex.findall(_local_string):
         print("checkpoint_2")
-        matc = cosRegex.search(s)
-        print("(COS) Handling match: " + matc.group())
-        return "-10"
-        pass
+        matc = cosRegex.findall(_local_string)
+        for matches in matc:
+            _local_string = cosRegex.sub("-10", _local_string)
+        _local_string = parseEqu(_local_string)
+        print("(COS) Handling match: " + str(matc))
+        print("COS changed string to -- " + _local_string)
 
-    # Compute 'priority' parenthesis
-    elif priorityRegex.match(s):
+
+
+    if cRegex.findall(_local_string):
         print("checkpoint_3")
-        matc = priorityRegex.search(s)
-        print("(Paren) Handling match: " + matc.group())
-        parseEqu(matc.group()[1:-1])
-        pass
+        matc = cRegex.findall(_local_string)
+        for matches in matc:
+            _local_string = cosRegex.sub(str(_local_string , _local_string))
+        _local_string = parseEqu(_local_string)
+        print("(COS) Handling match: " + str(matc))
+        print("COS changed string to -- " + _local_string)
 
+
+
+
+    """
+    # Compute 'priority' parenthesis
+    if priorityRegex.findall(_local_string):
+        print("checkpoint_3")
+        matc = priorityRegex.findall(_local_string)
+        for matches in matc:
+            _local_string = _local_string.replace(matches, parseEqu(matches))
+        print("(Paren) Handling match: " + str(matc.groups()))
+
+
+    
     # Compute basic operators
     else:
 
         # Check if the input is just a value
-        if re.match(r'^[0-9]*$', s):
+        if literalRegex.match(_local_string):
             print("checkpoint_4")
-            print("Literal: " + s + " found!")
-            return s
+            print("Literal: " + _local_string + " found!")
+            return _local_string
             
         else:
             print("checkpoint_5")
+
             # Split the equation at + or -
-            if ('+' in s) or ('-' in s):
-                Equsplit = re.compile(r'[\-\+]').split(s)
-            elif ('*' in s) or ('/' in s):
+            if ('+' in _local_string) or ('-' in _local_string):
+                Equsplit = re.compile(r'[\-\+]').split(_local_string)
+            elif ('*' in _local_string) or ('/' in _local_string):
                 # Split the equation at * or /
-                Equsplit = re.compile(r'[\*\/]').split(s)
+                Equsplit = re.compile(r'[\*\/]').split(_local_string)
             else:
-                pass
+                print("Something is wrong!")
 
             Operatorpos = len(Equsplit[0])
             print("Pos: " + str(Operatorpos))
-            Leftelement = s[:Operatorpos]
+            Leftelement = _local_string[:Operatorpos]
             print(Leftelement)
-            Rightelement = s[Operatorpos+1:]
+            Rightelement = _local_string[Operatorpos+1:]
             print(Rightelement)
-            op = s[Operatorpos]
+            op = _local_string[Operatorpos]
             print(op)
 
             debug = 0
@@ -84,10 +127,15 @@ def parseEqu(s):
                     return None
                 else:
                     return str(float(parseEqu(Leftelement)) / float(parseEqu(Rightelement)))
+    """
+    return _local_string
 
 
 
-string = 'sin(2232)-(1+1)'
-parseEqu(string)
+string = 'sin(cos(22)+2232)-(1+1)*cos(100)'
+#print(parseEqu(string))
+print(eval("1+1"))
+
+# Use python's internal parser to parse the simple things (+, -, /, *, ())
 
 
