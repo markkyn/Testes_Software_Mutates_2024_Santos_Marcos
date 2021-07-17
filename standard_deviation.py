ba0f48fec968b508f0bf854884a5e47293010e75
Arman <rmanaem@protonmail.ch>
2021-07-17 00:52:18 -0400

    Implemented mutator for the values attribute

--- standard_deviation.py
+++ standard_deviation.py
@@ -11,6 +11,15 @@ class StandardDeviation:
     def get_values(self):
         return self._values
 
+    def set_values(self, *values):
+        self._values = values
+        self._n_values = 0
+        for v in values:
+            self._n_values += 1
+        self._mean = self.__mean()
+        self._psd = self._population_standard_deviation()
+        self._ssd = self._sample_standard_deviation()
+
     def get_n_values(self):
         return self._n_values
     
@@ -55,6 +64,12 @@ class StandardDeviation:
         return (total / (self._n_values - 1)) ** 0.5
 
 std = StandardDeviation(10, 12, 23, 23, 16, 23, 21, 16)
+print(std.get_values())
+print(std.get_n_values())
+print(std.get_mean())
+print(std.get_psd())
+print(std.get_ssd())
+std.set_values(10, 12, 23, 23, 16, 23)
 print(std.get_values())
 print(std.get_n_values())
 print(std.get_mean())
