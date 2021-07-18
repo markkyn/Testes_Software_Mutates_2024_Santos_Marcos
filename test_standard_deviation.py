7d69bc3f4af3fb3518aee822ba868325e187e343
Arman <rmanaem@protonmail.ch>
2021-07-18 06:12:38 -0400

    Implemented test cases
    A test case implemented for mean, population standard deviation,
    and sample standard deviation functions

--- test_standard_deviation.py
+++ test_standard_deviation.py
@@ -1,13 +1,15 @@
+import decimal
 import sys
 from standard_deviation import StandardDeviation
 
+# Create a StandardDeviation object
 std = StandardDeviation(75, 89, 94, 13, 42, 77, 40)
 
 def test_mean():
-    assert(0 == 0)
+    assert(std.get_mean() == 61.42857142857)
 
 def test_population_standard_deviation():
-    pass
+    assert(std.get_psd() == 27.85091505476)
 
 def test_sample_standard_deviation():
-    pass
+    assert(std.get_ssd() == 30.08242644722)
