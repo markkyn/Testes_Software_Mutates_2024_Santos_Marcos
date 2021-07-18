a2bab9ec3a25718223195b2f4281adb1b2fffcad
Arman <rmanaem@protonmail.ch>
2021-07-18 06:34:34 -0400

    Modified tests Modified tests to evaluate the precision of the functions

--- test_standard_deviation.py
+++ test_standard_deviation.py
@@ -1,15 +1,13 @@
-import decimal
 import sys
 from standard_deviation import StandardDeviation
 
-# Create a StandardDeviation object
 std = StandardDeviation(75, 89, 94, 13, 42, 77, 40)
 
 def test_mean():
-    assert(std.get_mean() == 61.42857142857)
+    assert(abs(std.get_mean() - 61.42857142857) < 0.0000000001)
 
 def test_population_standard_deviation():
-    assert(std.get_psd() == 27.85091505476)
+    assert(abs(std.get_psd() - 27.85091505476) < 0.0000000001)
 
 def test_sample_standard_deviation():
-    assert(std.get_ssd() == 30.08242644722)
+    assert((std.get_ssd() - 30.08242644722) < 0.0000000001)
