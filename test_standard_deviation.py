a3263cc72c6df8d99fdb712627ef5b5efc81593e
Arman <rmanaem@protonmail.ch>
2021-07-18 06:48:56 -0400

    Documented the module

--- test_standard_deviation.py
+++ test_standard_deviation.py
@@ -1,8 +1,18 @@
+"""
+This module is a unit test for standard_deviation
+module and its functions.
+A StandardDeviation object is created using
+random numbers and used to check the precision
+of the __mean, _population_standard_deviation, 
+and _sample_standard_deviation functions.
+"""
+
 import sys
 from standard_deviation import StandardDeviation
 
 std = StandardDeviation(75, 89, 94, 13, 42, 77, 40)
 
+# Precision of 0.0000000001
 def test_mean():
     assert(abs(std.get_mean() - 61.42857142857) < 0.0000000001)
 
