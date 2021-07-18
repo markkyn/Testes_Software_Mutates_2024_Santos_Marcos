226ee5dd5e7ece9768010427c59264a4a5d48937
Arman <rmanaem@protonmail.ch>
2021-07-18 06:00:55 -0400

    Added unit test for standard_deviation module

---
+++ test_standard_deviation.py
@@ -0,0 +1,13 @@
+import sys
+from standard_deviation import StandardDeviation
+
+std = StandardDeviation(75, 89, 94, 13, 42, 77, 40)
+
+def test_mean():
+    assert(0 == 0)
+
+def test_population_standard_deviation():
+    pass
+
+def test_sample_standard_deviation():
+    pass
