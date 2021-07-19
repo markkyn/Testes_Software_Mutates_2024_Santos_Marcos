bda14d3da46daa88afd8a6e087187dbe9a507d75
Arman <rmanaem@protonmail.ch>
2021-07-19 10:54:13 -0400

    Added task module

---
+++ task_standard_deviation.py
@@ -0,0 +1,24 @@
+from standard_deviation import StandardDeviation
+
+
+std = StandardDeviation(10, 12, 23, 23, 16, 23, 21, 16)
+print("Set of values:", std.get_values())
+print("Mean: ", std.get_mean())
+print("Population Standard Deviation: ", std.get_psd())
+print("Sample Standard Deviation: ", std.get_ssd())
+
+# Remove the last two values
+std.set_values(10, 12, 23, 23, 16, 23)
+print("*******\n16 and 23 will be removed from the set.")
+print("Set of values:", std.get_values())
+print("Mean: ", std.get_mean())
+print("Population Standard Deviation: ", std.get_psd())
+print("Sample Standard Deviation: ", std.get_ssd())
+
+# Add the values back to the set
+std.add_values(21, 16)
+print("*******\n16 and 23 will be added back to the set.")
+print("Set of values:", std.get_values())
+print("Mean: ", std.get_mean())
+print("Population Standard Deviation: ", std.get_psd())
+print("Sample Standard Deviation: ", std.get_ssd())
