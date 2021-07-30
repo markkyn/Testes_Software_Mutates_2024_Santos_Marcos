d5a83b3e39f62d3145748f6013970336e36bfe3d
rmanaem <rmanaem@protonmail.ch>
2021-07-30 16:39:32 -0400

    Refactored code

--- deliverable3/src/test_standard_deviation.py
+++ deliverable3/src/test_standard_deviation.py
@@ -18,14 +18,19 @@ print('Set of values: 2**(1/2), ((3**(1/3))/2, 4**7, 13/4, sin(333), cos(963), 1
 std = StandardDeviation(2**(1./2), (3**(1./3))/2, 4**7, 13/4, math.sin(333), math.cos(963), 17)
 
 # Unit tests
+
+
 def test_mean():
     assert(abs(std.get_mean() - 2343.7535487876) < 0.0000000001)
+
 
 def test_population_standard_deviation():
     assert(abs(std.get_psd() - 5731.9093454445) < 0.0000000001)
 
+
 def test_sample_standard_deviation():
     assert((std.get_ssd() - 6191.1696957677) < 0.0000000001)
+
 
 print(std.get_mean())
 print(std.get_psd())
@@ -33,7 +38,8 @@ print(std.get_ssd())
 
 # Input test case
 if input('Would you like to test the module with your own input?(y/n)') == 'y':
-    numbers = input('Please provide numbers separated by comma to calculate the standard deviation \nExample: 7,9,20,20,13,20,18,13')
+    numbers = input('Please provide numbers separated by comma to calculate the standard deviation \n'
+                    'Example: 7,9,20,20,13,20,18,13')
     numbers = numbers.split(',')
 
     std = StandardDeviation()
