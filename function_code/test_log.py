0ec73b883f0258fada812a11831772152d0b4380
nicky <nicky@Nickys-MacBook-Pro.local>
2021-08-02 23:02:36 -0400

    Update structure

--- function_code/test_log.py
+++
@@ -1,9 +0,0 @@
-import math
-import log
-
-function = log.LogFunction(0, 0)
-function.set_values()
-answer = function.cal_log()
-print("For reasonable accuracy, we keep 10 decimals after the point.")
-print("The answer is %.10f" % answer)
-print("The built-in python log answer is %.10f" % math.log(function.argument, function.base))
