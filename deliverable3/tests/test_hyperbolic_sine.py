992296970f776e4dfa0dc9757b80b545633ee2d6
pooya khoshabi <44177095+pouyakhooshabi@users.noreply.github.com>
2021-08-04 09:54:43 -0400

    Add files via upload
    orgonizing files

---
+++ deliverable3/tests/test_hyperbolic_sine.py
@@ -0,0 +1,13 @@
+from hyperbolic_sine import HyperbolicSine
+import math
+
+# Using python's calculator to test if our calculator has enough accuracy or not.
+Sinh = HyperbolicSine(3)
+
+
+def test_answer():
+    assert(abs(Sinh.calculate_sinh() - math.sinh(Sinh.get_value())) < 0.0000000001)
+
+
+print(test_answer())
+# The accuracy was exactly what we wanted.
