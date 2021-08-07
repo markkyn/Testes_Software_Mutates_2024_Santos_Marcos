77ebfa4e824d8691d25a1c7e39dccfce05be0579
nickyk319 <35155640+nickyk319@users.noreply.github.com>
2021-08-06 23:27:27 -0400

    Update exceptions.py

--- deliverable3/src/exceptions.py
+++ deliverable3/src/exceptions.py
@@ -4,40 +4,20 @@ class Error(Exception):
 
 
 class BaseNumError1(Error):
-    """Exception raised for errors in the base number input equals to 1.
-
-    Attributes:
-        expression -- input expression in which the error occurred
-        message -- explanation of the error
-    """
+    """Exception raised for errors in the base number input equals to 1."""
     pass
 
 
 class BaseNumError2(Error):
-    """Exception raised for errors in the base number input less than or equals to zero.
-
-        Attributes:
-            expression -- input expression in which the error occurred
-            message -- explanation of the error
-        """
+    """Exception raised for errors in the base number input less than or equals to zero."""
     pass
 
 
 class ArgumentError(Error):
-    """Exception raised for errors in the argument number input less than or equals to 0.
-
-        Attributes:
-            expression -- input expression in which the error occurred
-            message -- explanation of the error
-        """
+    """Exception raised for errors in the argument number input less than or equals to 0."""
     pass
 
 
 class ZeroError(Error):
-    """Exception raised for errors when division by zero.
-
-        Attributes:
-            expression -- input expression in which the error occurred
-            message -- explanation of the error
-        """
+    """Exception raised for errors when division by zero."""
     pass
