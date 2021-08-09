91b541a10967de8d84bf8e4fddc4a155fd845b98
jjaha99 <jainiljaha@gmail.com>
2021-08-09 14:27:08 -0400

    Revert "Merge pull request #20 from rmanaem/rework"

---
+++ deliverable3/src/power_function.py
@@ -0,0 +1,44 @@
+class PowerFunction: 
+    # Receives the string "base"
+    # Sets the base of the function 
+    def set_base(self, base): 
+        # Check if the base is a real number
+        # If yes, separate both nums/denum of the fraction and return value
+        if("/" in str(base)):
+            try:
+                numerator = base[0:base.index('/')]
+                denumerator = base[base.index('/') + 1:]
+                self.base = float(numerator)/float(denumerator)
+            except ZeroDivisionError: 
+                print("Can't do division by zero!")
+        else:
+            self.base = float(base)
+
+    # Receives the string "exponent"
+    # Sets the base of the function 
+    def set_exponent(self, exponent):
+        # Check if the exponent is a real number
+        # If yes, separate both nums/denum of the fraction and return value
+        if("/" in str(exponent)):
+            try:
+                numerator = exponent[0:exponent.index('/')]
+                denumerator = exponent[exponent.index('/') + 1:]
+                self.exponent = float(numerator)/float(denumerator)
+            except ZeroDivisionError: 
+                print("Can't do division by zero!")
+        else:
+            self.exponent = float(exponent)
+    #PowerFunction parameters:
+    # base : Represents the input base (x) of the function
+    # exponent : Represents the input exponent (y) of the function 
+    def __init__(self, base, exponent):
+        self.set_base(base)
+        self.set_exponent(exponent)
+
+    # Does the operation
+    def exponentFunction(self):
+        return (self.base**self.exponent)
+
+    # Printing the base and exponent
+    def __str__(self):
+        return ("Base: " + str(self.base) + " Exponent: " + str(self.exponent))
