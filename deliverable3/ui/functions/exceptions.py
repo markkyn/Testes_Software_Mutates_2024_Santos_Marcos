3684320175829ac13e6fec6ab1dde65be0c4e776
Arman <rmanaem@protonmail.ch>
2023-04-05 17:51:46 -0400

    Added `exceptions.py` module to the ui app directory

---
+++ deliverable3/ui/functions/exceptions.py
@@ -0,0 +1,23 @@
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class BaseNumError1(Error):
+    """Exception raised for errors in the base number input equals to 1."""
+    pass
+
+
+class BaseNumError2(Error):
+    """Exception raised for errors in the base number input less than or equals to 0."""
+    pass
+
+
+class ArgumentError(Error):
+    """Exception raised for errors in the argument number input less than or equals to 0."""
+    pass
+
+
+class ZeroError(Error):
+    """Exception raised for errors when division by zero."""
+    pass
